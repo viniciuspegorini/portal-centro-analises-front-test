3ad05247bf06214738c20a9d8dfdf226c3566584
Guilherme Minozzi <minozzi60@gmail.com>
2023-04-16 21:34:46 -0300

    feat: add common errors

---
+++ src/services/api/errors/index.ts
@@ -0,0 +1,4 @@
+export * from '@/services/api/errors/accessDeniedError'
+export * from '@/services/api/errors/emailInUseError'
+export * from '@/services/api/errors/invalidCredentialsError'
+export * from '@/services/api/errors/unexpectedError'
