b8c82e44ad4ce2b7f284a4428d5f08cad49de5ea
Marco Antonio Rozo <69606427+marco-rozo@users.noreply.github.com>
2023-06-26 19:46:52 -0300

    fix: conflicts (#40)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,32 @@
 import React, { useContext } from "react";
 import styles from "./styles.module.scss";
 import { AuthContext } from "@/contexts";
-import { useHistory } from "@/hooks";
 import { IconButton } from "@mui/material";
 import { ExitToAppRounded } from "@material-ui/icons";
 import AuthService from "@/services/AuthService";
+import { useHistory } from "@/hooks";
 
 export function Header() {
   const { authenticatedUser } = useContext(AuthContext);
 
+  const { navigate } = useHistory();
+
+  const goToProfile = () => {
+    navigate("/profile");
+  };
+
   return (
     <div className={styles.container}>
-      <h2>Seja bem vindo, {authenticatedUser?.displayName}</h2>
-      <IconButton aria-label="delete" color="inherit" size="large" onClick={() => AuthService.logOut()} >
-        <ExitToAppRounded/>
+      <h2 onClick={goToProfile}>
+        Seja bem vindo, {authenticatedUser?.displayName}
+      </h2>
+      <IconButton
+        aria-label="delete"
+        color="inherit"
+        size="large"
+        onClick={() => AuthService.logOut()}
+      >
+        <ExitToAppRounded />
       </IconButton>
     </div>
   );
