8eec2bc74710f6fe09ab2d64906afbe800ffe5b3
Marco Antonio Rozo <69606427+marco-rozo@users.noreply.github.com>
2023-07-05 20:36:11 -0300

    Fix/perfil (#62)
    * fix: perfil
    
    * fix: removed not used style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,7 @@
 import React, { useContext } from "react";
 import styles from "./styles.module.scss";
 import { AuthContext } from "@/contexts";
-import { IconButton } from "@mui/material";
-import { ExitToAppRounded } from "@material-ui/icons";
+import { Button, Menu, MenuItem } from "@mui/material";
 import AuthService from "@/services/AuthService";
 import { useHistory } from "@/hooks";
 
@@ -15,19 +14,57 @@ export function Header() {
     navigate("/profile");
   };
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={styles.container}>
-      <h2 onClick={goToProfile}>
-        Seja bem vindo, {authenticatedUser?.displayName}
+      <h2>
+        Seja bem vindo,
+        <Button
+          aria-controls={open ? "demo-positioned-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          variant="text"
+          size="small"
+          onClick={handleClick}
+          style={{
+            color: "#3f51b5",
+          }}
+        >
+          {authenticatedUser?.displayName}
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem onClick={goToProfile}>Meu Perfil</MenuItem>
+          <MenuItem
+            onClick={() => {
+              AuthService.logOut();
+              handleClose;
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
       </h2>
-      <IconButton
-        aria-label="delete"
-        color="inherit"
-        size="large"
-        onClick={() => AuthService.logOut()}
-      >
-        <ExitToAppRounded />
-      </IconButton>
     </div>
   );
 }
